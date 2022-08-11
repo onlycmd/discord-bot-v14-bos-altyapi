eb0ff6a68b5fa2defd176c9d30a56215fd44a6a2
unknownCMD <107950550+unknownCMD@users.noreply.github.com>
2022-08-11 17:21:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "prefix": "/",
   "owner": "712328850331140176",
-  "token": "OTA3OTc1MTY0MDk0OTgwMTI3.GZru1N.yuC0Ei-7L9d4Oice0UzgsCFSx0NVpBQB1kb5j0"
+  "token": "TOKEN GİR"
 }
